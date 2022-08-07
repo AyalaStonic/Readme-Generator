2e50438aed60836a91a1cc00ed5f703a089677ed
Duvan <ayaladuvan876@gmail.com>
2022-08-07 15:57:39 -0400

    making some JS commands

--- index.js
+++ index.js
@@ -1,13 +1,94 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+// array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project? (REQUIRED)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project. (REQUIRED)',
+        validate: descInput => {
+            if (descInput) {
+                return true;
+            } else {
+                console.log('Please enter a description for your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation intructions for your project.'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions & examples for use. Option to add screenshots will be shown upon completion of this set of questions. (REQUIRED)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please enter any instructions for use!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Provide guidelines for contributing to this project.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide instructions on how to test the application.'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Please select a license to use for your project. (CHOOSE 1)',
+        choices: ['Apache 2.0', 'GNU GPL v3', 'MIT', 'No License', 'Skip']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username. (REQUIRED)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your e-mail address. (REQUIRED)',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your e-mail address.');
+                return false;
+            }
+        }
+    }
+];
